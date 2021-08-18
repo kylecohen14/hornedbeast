ddbd7b1601165880a3f887a1401eaea817d244e8
kylec <KYLECOHEN14@GMAIL.COM>
2021-08-17 20:28:36 -0400

    lab 02 finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
+import './style.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Data from './data.json';
+import { Component } from 'react';
 
-function App() {
+class App extends Component{
+  constructor (props) {
+    super(props);
+    this.state = {
+      beastData: Data,
+      // selectHorns: '',
+    };
+  }
+
+  render () {
   return (
     <div>
-      <Header title="Horned Beast Header"/>
-      <Main title="Horned Beast Main"/>
+      <Header title="Horned Beast"/>
+      <Main data={this.state.beastData}/>
       <Footer author="Author:Kyle"/>
     </div>
   );
 }
-
+}
 export default App;
