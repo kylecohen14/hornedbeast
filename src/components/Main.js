bbf346dfe82c7c1d6f89fdc77a8b4fc9cbef3988
kylec <KYLECOHEN14@GMAIL.COM>
2021-08-18 13:46:24 -0400

    netlify build

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,43 +4,21 @@ import Hornedbeast from './Hornedbeast';
 import {Row, Container} from 'react-bootstrap';
 
 
-// class Main extends React.Component {
-//   render () {
-//     return(
-//       <div>
-//         <Container>
-//           <Row xs={1} md={5} className="g-4">
-//           {this.props.data.map( (value) => {
-//           <Col>
-//           <Hornedbeast 
-//           title={value.title}
-//           image_url={value.image_url}
-//           description={value.description}
-//           horns={value.horns}/>
-//           </Col>
-//           })}
-//           </Row>
-//         </Container>
-//       </div>
-//     );
-//   }
-// }
 class Main extends React.Component {
   render () {
     return(
       <div>
-        
         <Container>
-         <Row xs={1} md={3} className="g-4">
-          {this.props.data.map( (value) => 
-          <Hornedbeast 
-          title={value.title}
-          image_url={value.image_url}
-          description={value.description}
-          horns={value.horns}/>
-          )}
+          <Row xs={1} md={3} className="g-4">
+            {this.props.data.map( (value) =>
+              <Hornedbeast
+                title={value.title}
+                image_url={value.image_url}
+                description={value.description}
+                horns={value.horns}/>
+            )}
           </Row>
-          </Container>
+        </Container>
       </div>
     );
   }
